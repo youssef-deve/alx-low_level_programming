85ea969aaa9e6e4b30fdc1f57abe170a8cfad5a6
youssef-deve <youssefkhachen@gmail.com>
2023-06-19 06:29:01 -0700

    fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,6 +15,7 @@ int main(void)
 	for (x = 0; x < 9; x++)
 	{
 		for (y = x + 1; y <= 9; y++)
+		{
 			putchar('0' + x);
 			putchar('0' + y);
 			if (x < 8 || y < 9)
@@ -22,6 +23,7 @@ int main(void)
 				putchar(',');
 				putchar(' ');
 			}
+		}
 	}
 	putchar('\n');
 	return (0);
