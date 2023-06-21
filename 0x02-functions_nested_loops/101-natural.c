abe7b143241f929a356e248ce47a470bc1fc61c7
youssef-deve <youssefkhachen@gmail.com>
2023-06-21 10:11:33 -0700

    fix

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check for start
+ *
+ * Description: computes the sum
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num / 3 == 0) || (num % 5 == 0))
+			sum += num;
+	}
+	printf("%d\n", sim);
+
+	return (0);
+}
