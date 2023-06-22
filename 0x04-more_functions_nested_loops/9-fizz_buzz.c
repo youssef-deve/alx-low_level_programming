e86b2cc83669d20fac59fde5261dbea86f1c6ffe
youssef-deve <youssefkhachen@gmail.com>
2023-06-22 04:01:57 -0700

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
-			prints("FizzBuzz ");
+			printf("FizzBuzz ");
 		else if (n % 3 == 0)
 			printf("Fizz ");
 		else if (n % 5 == 0)
@@ -23,5 +23,7 @@ int main(void)
 		else
 			printf("%d ", n);
 	}
+	putchar('\n');
+
 	return (0);
 }
