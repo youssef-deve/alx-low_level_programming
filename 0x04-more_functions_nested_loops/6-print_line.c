6be81b505465eba3ff0b3c19d9e73db04d004a15
youssef-deve <youssefkhachen@gmail.com>
2023-06-22 01:09:17 -0700

    fix it

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - function prints a line
+ *
+ * @n: input
+ *
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
